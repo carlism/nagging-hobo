643b1843123d2cff368be390dbbe37f5f9efbfda
cleiby <carl.leiby@gmail.com>
2011-02-10 23:53:31 -0500

    wrote a client

--- hobo.rb
+++ hobo.rb
@@ -1,22 +1,47 @@
+#!/usr/bin/env ruby
 require 'httparty'
 require 'builder'
+require 'optparse'
+require 'chronic'  # gem install aaronh-chronic for Ruby 1.9 compatibility
 
 class Hobo
   include HTTParty
-  base_uri 'http://localhost:1234'
+  base_uri 'http://nagging-hobo.heroku.com'
   format :xml
   debug_output $stdout
 
-  def self.post_job
+  def self.post_job(details)
     builder = Builder::XmlMarkup.new
-    options = { :body => builder.job { |j|
-      j.name 'test name'
-      j.message 'test message'
-      j.email 'carl.leiby@gmail.com'
-      j.trigger_at '2011-02-19T14:15:00-05:00'
+    post_data = { :body => builder.job { |j|
+      j.name details[:title]
+      j.message details[:msg]
+      j.email details[:email]
+      j.trigger_at details[:time]
     }}
-    puts post("/jobs", options)
+    puts post("/jobs", post_data)
   end
 end
 
-Hobo.post_job
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: hobo.rb [options]"
+  opts.on("-t", "--title TITLE", String, "Title of your push message") do |title|
+    options[:title] = title
+  end
+  opts.on("-m", "--message MESSAGE", String, "Push Message") do |msg|
+    options[:msg] = msg
+  end
+  opts.on("-e", "--email ADDRESS", String, "Email address of your boxcar account") do |email|
+    options[:email] = email
+  end
+  opts.on("-w", "--when TIME", String, "Push message at given time") do |time|
+    options[:time] = Chronic.parse(time)
+  end
+end.parse!
+raise ArgumentError, "title parameter required, see use -h for help" unless options[:title]
+raise ArgumentError, "message parameter required, see use -h for help" unless options[:msg]
+raise ArgumentError, "email parameter required, see use -h for help" unless options[:email]
+raise ArgumentError, "when/time parameter required, see use -h for help" unless options[:time]
+
+Hobo.post_job(options)
